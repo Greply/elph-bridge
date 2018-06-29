80fc9da07f3f261b4524d65d3c7acdc0822cefd1
Tanooj Luthra <tanoojl@gmail.com>
2018-06-28 21:14:07 -0700

    Removed unneeded files, and restructure provider.js

---
+++ README.md
@@ -0,0 +1,49 @@
+General flow of things
+
+Demo includes bundle.js (a wrapper around provider.js.  The provider.js loads up iframe.html (the actual web3 frame and login button), and iframe2.html (the modal confirmation iframe))
+
+- Demo calls new ElphProvider()
+- provider creates both iframes, and starts listening to all messages
+- "Login with Elph" is clicked on iframe.html
+- iframe.html initializes web3 with a seed phrase and password // TODO: ELPH API LOGIN WILL COME HERE
+- iframe.html finds iframe2.html
+- iframe2.html finds iframe.html
+- iframe.html sends an "AUTHENTICATED" back up to provider
+- provider is now ready to send web3 calls to iframe.html
+- provider sends standard calls to iframe.html with "REQUEST"
+- iframe.html forwards these calls to infura
+- iframe.html replies back to provider with "RESULT"
+- provider calls callback with result
+
+- provider sends conf req'd call to iframe.html with "REQUEST"
+- iframe.html realizes conf is needed, sends to iframe2.html with "CONFIRMATION"
+- iframe2.html pings provider with "SHOW_MODAL_IFRAME"
+- iframe2.html opens up modal with information
+- iframe2.html gets the user input to click on a button
+- iframe2.html pings provider with "HIDE_MODAL_IFRAME"
+- iframe2.html replies to iframe.html with "CONFIRMED" or "CANCELLED"
+- iframe.html manually checks "CONFIRMED" comes from same origin
+- iframe.html figures out what to do for each message ^
+- iframe.html follows same steps as unconf'd call.
+
+---
+
+Notes
+- demo needs to run on 127.0.0.1:8000
+- sdk needs to run on 127.0.0.1:9000
+- bundle.js needs to be included on every page because web3 needs to be on every page
+- on page redirects we need to use some kind of local storage to keep the seed around
+- login window needs to talk back somehow
+
+---
+
+To run in demo:
+- Load extension unpacked
+- Go to etherbots.io
+- See it needs metamask to connect
+- Click "Injecta" extention
+- See the "Login with Elph" button show up in top right
+- Click it
+- Connect to web3
+- Reattempt to go to etherbots.io
+- Magic.
--- provider.js
+++ provider.js
@@ -1,54 +1,128 @@
-var ElphProvider = (function () {
-    function ElphProvider(opts) {
-        this.authenticated = false;
-        this.initializeRequests();
-        this.initializeListener();
-        this.initializeIframe();
-    }
-    ElphProvider.prototype.initializeRequests = function () {
-        this.requests = {};
-    };
-    ElphProvider.prototype.initializeListener = function () {
-        var that = this;
-        window.addEventListener('message', function(e) {
+var ELPH_ORIGIN = 'http://127.0.0.1:9000';
+
+function ElphProvider() {
+   this.authenticated = false;
+   this.requests = {};
+   this.account = undefined;
+   this.net_version = undefined;
+   this.initializeListener();
+   this.initializeIframe();
+   this.initializeModalFrame(); 
+}
+
+ElphProvider.prototype.initializeListener = function () {
+    var that = this;
+    window.addEventListener('message', function(e) {
+        // TODO: add event origin check here.
+        if (e.origin === ELPH_ORIGIN) {
             console.log("Received message: ", e);
             if (e.data.type === "AUTHENTICATED") {
                 that.authenticated = true;
+                that.account = e.data.account;
+                that.net_version = e.data.net_version;
+                localStorage.setItem('elphAuthenticated', true);
             } else if (e.data.type === "RESULT") {
                 var callback = that.requests[e.data.payload.id].callback;
-                callback(e.data.error, e.data.result);
+                if (e.data.error) {
+                    callback(new Error(e.data.error), null);                    
+                } else {
+                    callback(null, e.data.result);
+                }
+            } else if (e.data.type === "SHOW_MODAL_IFRAME") {
+                // console.log("Should have opened modal iframe");
+                this.modalIframe.style.display = 'block';
+            } else if (e.data.type === "HIDE_MODAL_IFRAME") {
+                // console.log("Should have closed modal iframe");
+                this.modalIframe.style.display = 'none';
             } else {
                 console.log("got an unknown response back: ", e.data.type);
             }
-        });
-    };
-    ElphProvider.prototype.initializeIframe = function () {
-        var that = this;
-        this.iframe = document.createElement('iframe');
-        this.iframe.src = "file:///Users/tanooj/dev/sdk/iframe.html"
-        // this.frame.style.width = 0;
-        // this.frame.style.height = 0;
-        this.iframe.style.border = 0;
-        // this.iframe.style.border = none;
-        document.body.appendChild(this.iframe);
-    };
-    ElphProvider.prototype.sendAsync = function (payload, callback) {
-        this.requests[payload.id] = { payload: payload, callback: callback };
-        this.sendMessage({ type: "REQUEST", payload: payload });
-    };
-    ElphProvider.prototype.sendMessage = function (payload) {
-        var that = this;
-        if (!this.authenticated) {
-            console.log("Web3 not authenticated yet...");
-            window.setTimeout(function () {
-                that.sendMessage(payload)
-            }, 1000);
-        } else {
-            console.log("Sending payload: ", payload);
-            this.iframe.contentWindow.postMessage(payload, '*')            
         }
-    };
+    });
+};
+ElphProvider.prototype.initializeModalFrame = function () {
+    if (document.getElementById('modalIframe')) {
+        return true;
+    }
 
-    return ElphProvider;
-}());
+    this.modalIframe = document.createElement('iframe');
+    this.modalIframe.src = ELPH_ORIGIN + "/iframe2.html?" + Date.now().toString()
+    this.modalIframe.style.position = "absolute";
+    this.modalIframe.style.border = 0;
+    this.modalIframe.style.top = 0;
+    this.modalIframe.style.left = 0;
+    this.modalIframe.style.width = '100%';
+    this.modalIframe.style.height = '100%';
+    this.modalIframe.style.display = 'none';
+    this.modalIframe.style.zIndex = '10000000';
+    this.modalIframe.allowTransparency="true";
+    this.modalIframe.id = "modalIframe";
+    document.body.appendChild(this.modalIframe);
+};
+ElphProvider.prototype.initializeIframe = function () {
+    if (!localStorage.getItem('elphAuthenticated')) {
+        window.open(ELPH_ORIGIN + '/register', '_blank');
+    }
+
+    if (document.getElementById('web3Iframe')) {
+        return true;
+    }
+
+    this.iframe = document.createElement('iframe');
+    this.iframe.src = ELPH_ORIGIN + "/iframe.html?" + Date.now().toString()
+    this.iframe.style.border = 0;
+    this.iframe.style.position = "absolute";
+    this.iframe.style.height = 0;
+    this.iframe.style.width = 0;
+    this.iframe.style.zIndex = -100;
+    this.iframe.id = "web3Iframe";
+    document.body.appendChild(this.iframe);
+};
+ElphProvider.prototype.sendAsync = function (payload, callback) {
+    this.requests[payload.id] = { payload: payload, callback: callback };
+    this.sendMessage({ type: "REQUEST", payload: payload });
+};
+ElphProvider.prototype.send = function (payload) {
+    var method = payload.method;
+    var result;
+    switch(method) {
+        case "eth_accounts":
+            result = this.account ? [this.account] : []
+            break;
+        case "eth_coinbase":
+            result = this.account ? [this.account] : []
+            break;
+        case "eth_uninstallFilter":
+            // TODO
+            result = true;
+            break;
+        case "net_version":
+            result = this.net_version;
+            break;
+        default:
+            console.warn("Received synchronous method that is unsupported: ", payload)
+            throw new Error("Unsupported synchronous method");
+            break;
+    }
+    return {
+        id: payload.id,
+        jsonrpc: payload.jsonrpc,
+        result: result
+    };
+};
+ElphProvider.prototype.sendMessage = function (payload) {
+    var that = this;
+    if (!this.authenticated) {
+        // console.log("Web3 not authenticated yet...");
+        window.setTimeout(function () {
+            that.sendMessage(payload)
+        }, 1000);
+    } else {
+        console.log("Sending payload: ", payload);
+        this.iframe.contentWindow.postMessage(payload, ELPH_ORIGIN)            
+    }
+};
+
+ElphProvider.prototype.isElph = true;
+
 export { ElphProvider };

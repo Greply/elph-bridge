9c9cea8902112edb5a8a4ab66d175465515a1f96
Tanooj Luthra <tanoojl@gmail.com>
2018-07-25 20:43:34 -0700

    Private-ize top level package.json

--- tools/build.js
+++ tools/build.js
@@ -35,8 +35,6 @@ promise = promise.then(() => {
   delete pkg.private;
   delete pkg.devDependencies;
   delete pkg.scripts;
-  delete pkg.eslintConfig;
-  delete pkg.babel;
   fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
   fs.writeFileSync('dist/LICENSE.txt', fs.readFileSync('LICENSE.txt', 'utf-8'), 'utf-8');
   fs.writeFileSync('dist/README.md', fs.readFileSync('README.md', 'utf-8'), 'utf-8');
